85a9d018ef9ca1dec808a9d3c9b262732a1d255d
Guido Zockoll <gzockoll@gmail.com>
2018-08-10 14:42:17 +0200

    Initial Import

---
+++ src/main/java/com/example/demo/WebappExampleProcessApplication.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import lombok.RequiredArgsConstructor;
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.spring.boot.starter.event.PostDeployEvent;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableCaching
+@RequiredArgsConstructor
+@EnableScheduling
+public class WebappExampleProcessApplication {
+    private final RuntimeService runtimeService;
+
+    public static void main(String... args) {
+        SpringApplication.run(WebappExampleProcessApplication.class, args);
+    }
+
+    @EventListener
+    private void processPostDeploy(PostDeployEvent event) {
+        runtimeService.startProcessInstanceByKey("loanApproval");
+    }
+}
