6297c220f03e4abd2cc426d2dc994fb17423b7c6
Guido Zockoll <gzockoll@gmail.com>
2018-08-10 14:49:20 +0200

    Initial Import

---
+++ src/main/java/com/example/demo/CacheEvictController.java
@@ -0,0 +1,52 @@
+package com.example.demo;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.camunda.bpm.engine.impl.history.event.HistoricDetailEventEntity;
+import org.camunda.bpm.engine.impl.history.event.HistoricTaskInstanceEventEntity;
+import org.camunda.bpm.engine.impl.history.event.HistoryEvent;
+import org.camunda.bpm.engine.impl.history.handler.HistoryEventHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.CacheManager;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+@Slf4j
+public class CacheEvictController implements HistoryEventHandler {
+    private final CacheManager cacheManger;
+
+    @Lazy
+    @Autowired
+    private TaskController taskController;
+
+    @Override
+    public void handleEvent(HistoryEvent historyEvent) {
+        log.debug(historyEvent.toString());
+
+        String taskId = null;
+
+        if (historyEvent instanceof HistoricDetailEventEntity) {
+            taskId = (((HistoricDetailEventEntity) historyEvent).getTaskId());
+        }
+
+        if (historyEvent instanceof HistoricTaskInstanceEventEntity) {
+            taskId = (((HistoricTaskInstanceEventEntity) historyEvent).getTaskId());
+        }
+
+        // add more events as needed
+
+        if (taskId!=null) {
+            // taskController.evict(taskId);
+            taskController.getTaskFromCamundaById(taskId);
+        }
+    }
+
+    @Override
+    public void handleEvents(List<HistoryEvent> historyEvents) {
+        historyEvents.forEach(e -> handleEvent(e));
+    }
+}
---
+++ src/main/java/com/example/demo/TaskController.java
@@ -0,0 +1,62 @@
+package com.example.demo;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.camunda.bpm.engine.TaskService;
+import org.camunda.bpm.engine.task.Task;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import static com.example.demo.HazelcastConfig.TASKS_CACHE;
+
+@Lazy
+@Service
+@RequiredArgsConstructor
+@CacheConfig(cacheNames = {TASKS_CACHE})
+@Slf4j
+public class TaskController {
+    private final TaskService taskService;
+    private final CacheManager cacheManager;
+
+    /**
+     * This is the normal method to get a single task
+     * It is served from the cache if present.
+     * @param id
+     * @return Camunda Task Object
+     */
+    @Cacheable
+    public Task getTaskById(String id) {
+        return taskService.createTaskQuery().taskId(id).singleResult();
+    }
+
+    /**
+     * This method evicts a single task object from the cache
+     * @param id
+     */
+    @CacheEvict(key="#id")
+    public void evict(String id) {
+        log.debug("Evict: " + id);
+    }
+
+    /**
+     * This method does a forced reload from camunda and puts the result in the cache.
+     * Used to refresh from the event listener.
+     * Sometimes it happens that the event listner fires with a new task id but the
+     * API does not find it :(
+     * @param id
+     * @return Camunda Task Object
+     */
+    @CachePut
+    @Async
+    public Task getTaskFromCamundaById(String id) {
+        Task task = taskService.createTaskQuery().taskId(id).singleResult();
+        log.debug(String.format("Updated Task %s: %s",id,task));
+        return task;
+    }
+}
